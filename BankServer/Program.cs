1d3f6da5c893df4ba2fcf9c5a73fd3201e25a60b
Nikolai <nikolaiviby3@gmail.com>
2019-09-02 17:08:08 +0200

    init

--- BankServer/Program.cs
+++ BankServer/Program.cs
@@ -11,72 +11,83 @@ namespace BankServer
 {
     class Program
     {
-        private static bool _resetLoop = true;
+
 
         static void Main(string[] args)
         {
             CustomerLedger.Ledger.Add(1, 100000);
             CustomerLedger.Ledger.Add(2, 15356);
             CustomerLedger.Ledger.Add(3, -356);
-
             IPAddress ip = IPAddress.Parse("127.0.0.1");
 
             TcpListener serverSocket = new TcpListener(ip, 6789);
             serverSocket.Start();
-
             Console.WriteLine("Server started");
-           
+
+
+
+
+            while (true)
+            {
                 TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-                //Socket connectionSocket = serverSocket.AcceptSocket();
                 Console.WriteLine("Server activated");
-
-                Stream ns = connectionSocket.GetStream();
-                // Stream ns = new NetworkStream(connectionSocket);
-
-                StreamReader sr = new StreamReader(ns);
-                StreamWriter sw = new StreamWriter(ns);
-                sw.AutoFlush = true; // enable automatic flushing
-
-                string message;
-                message = "";
-                message = sr.ReadLine();
-                string answer = "";
-                while (true)
-                {
-
-                    Console.WriteLine("Client: " + message);
-                    if (message != null)
-                    {
-                        if (message == "quit")
-                        {
-                            break;
-                        }
-
-                        string[] splitmessage = message.Split(' ');
-                        string sId = splitmessage[0];
-                        int id = Int32.Parse(sId);
-                        string amount = splitmessage[1];
-                        double amountD = double.Parse(amount);
-
-                        CustomerLedger.Ledger[id] += amountD;
-                        answer = CustomerLedger.Ledger[id].ToString();
-                        sw.WriteLine(answer);
-
-                       
-
-                    }
-
-                    message = sr.ReadLine();
-                    // connection skal lukkes her, og der skal resettes
-                }
-
-                //ns.Close();
-                connectionSocket.Close();
-                
-
-            
+                Server s = new Server(connectionSocket);
+                Task.Factory.StartNew(() => s.StartNewConnection());
 
 
+
+                //TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                ////Socket connectionSocket = serverSocket.AcceptSocket();
+                //Console.WriteLine("Server activated");
+
+                //Stream ns = connectionSocket.GetStream();
+                //// Stream ns = new NetworkStream(connectionSocket);
+
+                //StreamReader sr = new StreamReader(ns);
+                //StreamWriter sw = new StreamWriter(ns);
+                //sw.AutoFlush = true; // enable automatic flushing
+
+                //string message;
+                //message = "";
+                //message = sr.ReadLine();
+                //string answer = "";
+                //while (true)
+                //{
+
+                //    Console.WriteLine("Client: " + message);
+                //    if (message != null)
+                //    {
+                //        if (message == "quit")
+                //        {
+                //            break;
+                //        }
+
+                //        string[] splitmessage = message.Split(' ');
+                //        string sId = splitmessage[0];
+                //        int id = Int32.Parse(sId);
+                //        string amount = splitmessage[1];
+                //        double amountD = double.Parse(amount);
+
+                //        CustomerLedger.Ledger[id] += amountD;
+                //        answer = CustomerLedger.Ledger[id].ToString();
+                //        sw.WriteLine(answer);
+
+
+
+                //    }
+
+                //    message = sr.ReadLine();
+                //    // connection skal lukkes her, og der skal resettes
+                //}
+
+                ////ns.Close();
+                //connectionSocket.Close();
+
+
+
+
+
+            }
         }
     }
 }
---
+++ BankServer/server.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+
+namespace BankServer
+{
+    public class Server
+    {
+        private TcpClient serverSocket;
+        public Server(TcpClient listerner)
+        {
+            serverSocket = listerner;
+        }
+        internal void StartNewConnection()
+        {
+            
+            //Socket connectionSocket = serverSocket.AcceptSocket();
+           
+
+            Stream ns = serverSocket.GetStream();
+            // Stream ns = new NetworkStream(connectionSocket);
+
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message;
+            message = "";
+            message = sr.ReadLine();
+            string answer = "";
+            while (true)
+            {
+
+                Console.WriteLine("Client: " + message);
+                if (message != null)
+                {
+                    if (message == "quit")
+                    {
+                        break;
+                    }
+
+                    string[] splitmessage = message.Split(' ');
+                    string sId = splitmessage[0];
+                    int id = Int32.Parse(sId);
+                    string amount = splitmessage[1];
+                    double amountD = double.Parse(amount);
+
+                    CustomerLedger.Ledger[id] += amountD;
+                    answer = CustomerLedger.Ledger[id].ToString();
+                    sw.WriteLine(answer);
+
+
+
+                }
+
+                message = sr.ReadLine();
+                // connection skal lukkes her, og der skal resettes
+            }
+
+            //ns.Close();
+            serverSocket.Close();
+            
+        }
+        
+        
+
+        
+
+    }
+}
